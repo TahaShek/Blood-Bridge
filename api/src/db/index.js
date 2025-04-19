7f0bc938bd7f96d046329f8ea7eac6882ff9a0f3
steadyTurtle2002 <steadyTurtule2002@gmail.com>
2025-04-19 22:30:02 +0500

    feat:auth ap    implemented

---
+++ api/src/db/index.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import { DB_NAME } from "../constants/constants.js"
+
+dotenv.config({
+    path: "../.env"
+});
+
+export const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URL}${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to Database", error);
+        process.exit(1);
+    }
+};
